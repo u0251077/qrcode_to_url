033677ed68e0a8df05fc89663e58635f31489c55
Pei Ying <u0251077@gmail.com>
2024-09-03 23:50:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,7 @@
 import streamlit as st
-from PIL import Image
-import cv2
-import numpy as np
+from streamlit_qrcode_scanner import qrcode_scanner
 
-def scan_qrcode(image):
-    # 將 PIL 圖片轉換為 OpenCV 圖像格式
-    image = np.array(image)
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    
-    # 使用 OpenCV 的 QRCodeDetector 來解碼 QR 碼
-    detector = cv2.QRCodeDetector()
-    decoded_text, points, qr_code = detector(gray)
-    
-    return decoded_text
+qr_code = qrcode_scanner(key='qrcode_scanner')
 
-def main():
-    st.title('QR Code Scanner')
-
-    uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg", "jpeg"])
-    
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        
-        with st.spinner('Scanning QR code...'):
-            decoded_text = scan_qrcode(image)
-            
-        if decoded_text:
-            st.subheader("QR Code Data:")
-            st.write(decoded_text)
-        else:
-            st.write("No QR Code detected.")
-
-if __name__ == "__main__":
-    main()
+if qr_code:
+    st.write(qr_code)
